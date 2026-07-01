5734896d236082d6979c6a84356e1d3519510f07
Antonio Eduardo Moreira Oliveira <119017563+Antonio-Eduardo@users.noreply.github.com>
2026-07-01 20:00:54 -0300

    feat: grafico de analise para tipo de devedor por UF

--- src/analise.py
+++ src/analise.py
@@ -1,6 +1,7 @@
 from load import abrirConexao
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 engine = abrirConexao()
 
@@ -15,9 +16,20 @@ query_devedor_tipo = """
     GROUP BY "TIPO_DEVEDOR"
 """
 query_devedor_uf = """
-    SELECT "TIPO_DEVEDOR", "UF_DEVEDOR", COUNT(*) AS total
+    SELECT "UF_DEVEDOR", COUNT(*) AS total
     FROM divida_ativa
-    GROUP BY "TIPO_DEVEDOR","UF_DEVEDOR"
+    GROUP BY "UF_DEVEDOR"
+    ORDER BY total DESC
+"""
+query_valor_medio_uf = """
+    SELECT "UF_DEVEDOR", AVG("VALOR_CONSOLIDADO") AS valor_medio
+    FROM divida_ativa
+    GROUP BY "UF_DEVEDOR"
+    """
+query_valor_medio_tipo_pessoa = """
+    SELECT "TIPO_PESSOA", AVG("VALOR_CONSOLIDADO) AS valor_medio
+    FROM divida_ativa
+    GROUP BY "TIPO_PESSOA"
 """
 colunas = pd.read_sql(query_column_name,engine)
 devedorPorTipo = pd.read_sql(query_devedor_tipo,engine)
@@ -25,8 +37,9 @@ devedorPorUf = pd.read_sql(query_devedor_uf,engine)
 
 print(colunas)
 print(devedorPorTipo)
-print(devedorPorUf)
+print(devedorPorUf["UF_DEVEDOR"].unique())
 
+#Grafico de barras para quantidade de devedores por tipo
 total_geral = devedorPorTipo["total"].sum()
 bars = plt.bar(devedorPorTipo["TIPO_DEVEDOR"], devedorPorTipo["total"])
 for bar in bars:
@@ -43,4 +56,42 @@ plt.xlabel("Tipo de devedor")
 plt.ylabel("Total de registros")
 plt.ticklabel_format(style="plain", axis="y")
 plt.yscale("log")
+plt.show()
+
+#Grafico para visualização da quantidade de devedores por UF
+bars = plt.bar(devedorPorUf["UF_DEVEDOR"], devedorPorUf["total"])
+for bar in bars:
+    altura = bar.get_height()
+    plt.text(
+        bar.get_x() + bar.get_width()/2,
+        altura,
+        f"{altura:,.0f}",
+        ha="center",va="bottom",
+        fontsize=8, rotation=10
+    )
+plt.title("Quantidade de devedores por UF")
+plt.xlabel("UF do devedor")
+plt.ylabel("Total de registros")
+plt.ticklabel_format(style="plain", axis="y")
+plt.show()
+
+#Grafico tipo devedor por UF
+query_tipo_devedor_por_uf = """
+    SELECT "UF_DEVEDOR", "TIPO_PESSOA", COUNT(*) as total
+    FROM divida_ativa
+    GROUP BY "UF_DEVEDOR","TIPO_PESSOA"
+"""
+tipoDevedor_UF = pd.read_sql(query_tipo_devedor_por_uf,engine)
+pivot =tipoDevedor_UF.pivot(index="UF_DEVEDOR", columns="TIPO_PESSOA",values="total")
+x = np.arange(len(pivot.index))
+largura = 0.4
+barPF = plt.bar(x - largura/2, pivot["Pessoa física"], largura,label="PF")
+barPJ = plt.bar(x + largura/2, pivot["Pessoa jurídica"], largura, label="PJ")
+plt.xticks(x,pivot.index)
+plt.title("Tipo devedor por UF")
+plt.xlabel("UF do devedor")
+plt.ylabel("Total de devedores")
+plt.ticklabel_format(style="plain", axis="y")
+plt.yscale("log")
+plt.legend()
 plt.show()
--- src/extract.py
+++ src/extract.py
@@ -9,7 +9,7 @@ def extrair():
 
 #Tive que criar uma nova função pra extrair arquivo por arquivo
 #em um loop, a função extrair() deu erro de memoria pelo tamanho
-#total dos 6 csvs
+#total dos 6 csv
 
 def extrair_arquivo(arquivo):
     df = pd.read_csv(arquivo, sep=";",encoding="latin1")
